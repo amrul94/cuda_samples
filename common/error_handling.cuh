0238605cf44a6fb03b0ebf3ef9082dd34df297aa
Amrulla Akhmedov <amrul094@gmail.com>
2024-04-19 23:49:08 +0300

    expanded vector_add

--- common/error_handling.cuh
+++ common/error_handling.cuh
@@ -9,18 +9,18 @@
 
 static void handleError(cudaError_t err, const char *file, int line) {
   if (err != cudaSuccess) {
-    printf("%s in %s at line %d\n", cudaGetErrorString(err), file, line);
-    exit(EXIT_FAILURE);
+    std::printf("%s in %s at line %d\n", cudaGetErrorString(err), file, line);
+    std::exit(EXIT_FAILURE);
   }
 }
 
 static void handleError(void *pointer, const char *file, int line) {
   if (pointer == nullptr) {
-    printf("Host memory failed  in %s at line %d\n", file, line);
-    exit(EXIT_FAILURE);
+    std::printf("Host memory failed  in %s at line %d\n", file, line);
+    std::exit(EXIT_FAILURE);
   }
 }
 
-#define HANDLE_ERROR(err) (handleError(err, __FILE__, __LINE__))
+#define HANDLE_ERROR(ret) handleError(ret, __FILE__, __LINE__)
 
 #endif // CUDA_SAMPLES_COMMON_ERROR_HANDLING_HPP
--- samples/simple/vector_add.cu
+++ samples/simple/vector_add.cu
@@ -1,43 +1,84 @@
 //
 // Created by amrulla on 19.04.2024.
 //
+
+#include <format>
+#include <iostream>
+
 #include "common/error_handling.cuh"
 
-constexpr size_t N = 10;
+__host__ size_t getVectorSize(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cerr << "Incorrect number of arguments: " << argc - 1
+              << " but expect 1" << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
 
-__global__ void add(const int *a, const int *b, int *c) {
-  unsigned int tid = blockIdx.x;
-  if (tid < N) {
+  char *end = nullptr;
+  size_t ret = strtoul(argv[1], &end, 10);
+
+  if (errno == ERANGE) {
+    std::perror("!! Problem is -> ");
+    std::exit(EXIT_FAILURE);
+  } else if (ret) {
+    std::cout << "The number is " << ret << std::endl;
+    return ret;
+  } else {
+    std::cerr << "No number found input is -> " << argv[1] << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+}
+
+__global__ void vectorAddKernel(const float *a, const float *b, float *c,
+                                size_t n) {
+  unsigned int tid = blockIdx.x * blockDim.x + threadIdx.x;
+  if (tid < n) {
     c[tid] = a[tid] + b[tid];
   }
 }
 
-int main() {
-  int a[N], b[N], c[N];
-  int *dev_a, *dev_b, *dev_c;
+__host__ void vectorAdd(const float *h_a, const float *h_b, float *h_c,
+                        size_t n) {
+  float *d_a, *d_b, *d_c;
+  const size_t size = n * sizeof(float);
 
-  const size_t size = N * sizeof(int);
-  HANDLE_ERROR(cudaMalloc((void **)&dev_a, size));
-  HANDLE_ERROR(cudaMalloc((void **)&dev_b, size));
-  HANDLE_ERROR(cudaMalloc((void **)&dev_c, size));
+  HANDLE_ERROR(cudaMalloc((void **)&d_a, size));
+  HANDLE_ERROR(cudaMalloc((void **)&d_b, size));
+  HANDLE_ERROR(cudaMalloc((void **)&d_c, size));
 
-  for (int i = 0; i < N; ++i) {
-    a[i] = -i;
-    b[i] = i * i;
+  HANDLE_ERROR(cudaMemcpy(d_a, h_a, size, cudaMemcpyHostToDevice));
+  HANDLE_ERROR(cudaMemcpy(d_b, h_b, size, cudaMemcpyHostToDevice));
+
+  size_t grid_dim = std::ceil(static_cast<float>(n) / 256.0);
+  vectorAddKernel<<<grid_dim, 256>>>(d_a, d_b, d_c, n);
+
+  HANDLE_ERROR(cudaMemcpy(h_c, d_c, size, cudaMemcpyDeviceToHost));
+
+  cudaFree(d_a);
+  cudaFree(d_b);
+  cudaFree(d_c);
+}
+
+int main(int argc, char *argv[]) {
+
+  size_t n = getVectorSize(argc, argv);
+
+  auto *a = new float[n];
+  auto *b = new float[n];
+  auto *c = new float[n];
+
+  for (int i = 0; i < n; ++i) {
+    a[i] = static_cast<float>(-i);
+    b[i] = static_cast<float>(i * i);
   }
 
-  HANDLE_ERROR(cudaMemcpy(dev_a, a, size, cudaMemcpyHostToDevice));
-  HANDLE_ERROR(cudaMemcpy(dev_b, b, size, cudaMemcpyHostToDevice));
+  vectorAdd(a, b, c, n);
 
-  add<<<N, 1>>>(dev_a, dev_b, dev_c);
-
-  HANDLE_ERROR(cudaMemcpy(c, dev_c, size, cudaMemcpyDeviceToHost));
-
-  for (int i = 0; i < N; ++i) {
-    printf("%d + %d = %d\n", a[i], b[i], c[i]);
+  for (int i = 0; i < n; ++i) {
+    std::printf("%f + %f = %f\n", a[i], b[i], c[i]);
   }
 
-  cudaFree(dev_a);
-  cudaFree(dev_b);
-  cudaFree(dev_c);
+  delete[] a;
+  delete[] b;
+  delete[] c;
 }
