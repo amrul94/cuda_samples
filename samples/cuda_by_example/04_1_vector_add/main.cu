71440db888008c74b868fb2efad687e4748bc217
Amrulla Akhmedov <amrul094@gmail.com>
2024-10-02 12:13:39 +0300

    add copyright info

--- samples/cuda_by_example/04_1_vector_add/main.cu
+++ samples/cuda_by_example/04_1_vector_add/main.cu
@@ -1,6 +1,17 @@
-//
-// Created by amrulla on 19.04.2024.
-//
+/*
+* Copyright 1993-2010 NVIDIA Corporation.  All rights reserved.
+*
+* NVIDIA Corporation and its licensors retain all intellectual property and
+* proprietary rights in and to this software and related documentation.
+* Any use, reproduction, disclosure, or distribution of this software
+* and related documentation without an express license agreement from
+* NVIDIA Corporation is strictly prohibited.
+*
+* Please refer to the applicable NVIDIA end user license agreement (EULA)
+* associated with this source code for terms and conditions that govern
+* your use of this NVIDIA software.
+*
+*/
 
 #include <format>
 #include <iostream>
