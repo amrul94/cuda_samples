03203a5be23fc6542a5362d29be97d823d33b214
Amrulla Akhmedov <amrul094@gmail.com>
2024-10-02 15:24:15 +0300

    rafactoring handle error file

--- common/error_handling.cuh
+++ common/error_handling.cuh
@@ -2,8 +2,8 @@
 // Created by amrulla on 18.04.2024.
 //
 
-#ifndef CUDA_SAMPLES_COMMON_ERROR_HANDLING_HPP
-#define CUDA_SAMPLES_COMMON_ERROR_HANDLING_HPP
+#ifndef CUDA_SAMPLES_COMMON_ERROR_HANDLING_CUH
+#define CUDA_SAMPLES_COMMON_ERROR_HANDLING_CUH
 
 #include <cstdio>
 
@@ -16,11 +16,11 @@ static void handleError(cudaError_t err, const char *file, int line) {
 
 static void handleError(void *pointer, const char *file, int line) {
   if (pointer == nullptr) {
-    std::printf("Host memory failed  in %s at line %d\n", file, line);
+    std::printf("Host memory failed in %s at line %d\n", file, line);
     std::exit(EXIT_FAILURE);
   }
 }
 
 #define HANDLE_ERROR(ret) handleError(ret, __FILE__, __LINE__)
 
-#endif // CUDA_SAMPLES_COMMON_ERROR_HANDLING_HPP
+#endif // CUDA_SAMPLES_COMMON_ERROR_HANDLING_CUH
