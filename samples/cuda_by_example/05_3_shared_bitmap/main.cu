55d2e0eed7a04c9d3c570e9c852720bf29a9557f
Amrulla Akhmedov <amrul094@gmail.com>
2024-10-12 13:41:09 +0300

    refactoring shared bitmap sample

--- samples/cuda_by_example/05_3_shared_bitmap/main.cu
+++ samples/cuda_by_example/05_3_shared_bitmap/main.cu
@@ -20,23 +20,30 @@
 constexpr unsigned int DIM = 1024;
 constexpr float PI = 3.1415926535897932f;
 
+__device__ float partialCalc(unsigned int v, float period) {
+  const auto fv = static_cast<float>(v);
+  return sinf(fv * 2.0f * PI / period) + 1.0f;
+}
+
 __global__ void kernel(unsigned char *ptr) {
-  int x = threadIdx.x + blockIdx.x * blockDim.x;
-  int y = threadIdx.y + blockIdx.y * blockDim.y;
-  int offset = x + y * blockDim.x * gridDim.x;
+  unsigned int x = threadIdx.x + blockIdx.x * blockDim.x;
+  unsigned int y = threadIdx.y + blockIdx.y * blockDim.y;
+  unsigned int offset = x + y * blockDim.x * gridDim.x;
 
   __shared__ float shared[16][16];
 
   const float period = 128.0f;
 
-  shared[threadIdx.x][threadIdx.y] =
-      255 * (sinf(x * 2.0f * PI / period) + 1.0f) *
-            (sinf(y * 2.0f * PI / period) + 1.0f) / 4.0f;
+  const float vx = partialCalc(x, period);
+  const float vy = partialCalc(y, period);
+  shared[threadIdx.x][threadIdx.y] = 255 * vx * vy / 4.0f;
 
   __syncthreads();
 
+  const float color = shared[15 - threadIdx.x][15 - threadIdx.y];
+
   ptr[offset * 4 + 0] = 0;
-  ptr[offset * 4 + 1] = shared[15 - threadIdx.x][15 - threadIdx.y];
+  ptr[offset * 4 + 1] = static_cast<unsigned char>(color);
   ptr[offset * 4 + 2] = 0;
   ptr[offset * 4 + 3] = 255;
 }
